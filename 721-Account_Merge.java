8c4696bf8ab4c95aa82f064632320346a5190ed5
IceTrawberry <36254958+IceTrawberry@users.noreply.github.com>
2018-12-25 15:11:25 -0600

    1.Union-Find. 2.DFS

--- 721-Account_Merge.java
+++ 721-Account_Merge.java
@@ -1,3 +1,5 @@
+//###1 Union Find
+
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         Map<String, String> parents=new HashMap<>();
@@ -43,4 +45,46 @@ class Solution {
         return map.get(target)==target?target:find(map.get(target),map);
     }
     
+}
+
+
+//###2 DFS
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String,String> names=new HashMap<>();
+        for(List<String> acc:accounts){
+            for(int i=1;i<acc.size();i++)
+                names.put(acc.get(i),acc.get(0));
+        }
+        Map<String,HashSet<String>> graph=new HashMap<>();
+        for(List<String>acc:accounts){
+            for(int i=1;i<acc.size();i++){
+                if(!graph.containsKey(acc.get(i)))
+                    graph.put(acc.get(i),new HashSet<>());
+                if(i==1) continue;
+                graph.get(acc.get(i)).add(acc.get(i-1));
+                graph.get(acc.get(i-1)).add(acc.get(i));
+            }
+        }
+        
+        Set<String> emails=new HashSet<>();
+        List<List<String>>ls=new LinkedList<>();
+        for(String email:names.keySet()){
+            if(emails.add(email)){
+                List<String> tmp= new LinkedList<>();
+                dfs(graph,emails,email,tmp);
+                Collections.sort(tmp);
+                tmp.add(0,names.get(email));
+                ls.add(tmp);
+            }
+        }
+        return ls;
+    }
+    public void dfs(Map<String,HashSet<String>> graph, Set<String> emails, String email, List<String> tmp){
+        tmp.add(email);
+        for(String next: graph.get(email)){
+            if(emails.add(next))
+                dfs(graph,emails,next,tmp);
+        }
+    }
 }
