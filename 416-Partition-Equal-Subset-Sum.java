1942429305fc1f3b90b3612763739dd231efc9c9
IceTrawberry <36254958+IceTrawberry@users.noreply.github.com>
2018-12-30 17:37:13 -0600

    Create 416-Partition-Equal-Subset-Sum.java

---
+++ 416-Partition-Equal-Subset-Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int num:nums)
+            sum+=num;
+        if(sum%2==1) return false;
+        sum/=2;
+       
+        boolean []dp=new boolean [sum+1];
+        dp[0]=true;
+        for(int num:nums){
+            for(int i=sum;i>=num;i--)
+                dp[i]=dp[i-num]||dp[i];
+        }
+      
+        return dp[sum];
+    }
+}
